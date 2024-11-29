237344227ecbc995c09de810a738147867fa96b9
ssamko0911 <ssamko091184@gmail.com>
2024-11-29 18:55:28 +0000

    Create migration

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,17 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        dump($request);
+
+        $great = '';
+        if ($name = $request->query->get('hello')) {
+            $great = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+
         return new Response(<<<EOF
             <html>
                 <body>
+                    $great
                     <img src="/images/under-construction.gif" />
                </body>
             </html>
